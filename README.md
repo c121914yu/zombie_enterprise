a526fc2f9a57186ccdcd5569cfb41c3e2588fd3a
yjl <545436317@qq.com>
2020-04-27 23:26:47 +0800

    README.md

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # 僵尸企业画像
 
 ## 指令
-
-> npm install - 安装依赖
-> npm install name -S   - 安装包
-> npm run serve - 启动开发环境
-> npm run build - 打包
+ 
+> npm install - 安装依赖  
+> npm install name -S   - 安装包  
+> npm run serve - 启动开发环境  
+> npm run build - 打包  s
 
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,19 @@
 module.exports = {
 	outputDir: "dist", //输出目录
-	assetsDir: 'assets' ,//输入静态资源的目录
+	assetsDir: 'assets', //输入静态资源的目录
 	publicPath: './', //部署时基本url
 	indexPath: "index.html", //指定index.html输出路径(相对于dist)
 	lintOnSave: false, //是否开启eslint保存检测
+	filenameHashing: true,
+	runtimeCompiler: false,
+	productionSourceMap: true,
 	devServer: { //生产环境配置
 		open: false,
 		host: 'localhost',
 		port: 8080,
 		https: false,
 		hotOnly: false,
-	  proxy: {
+		proxy: {
 			'/api': {
 				target: "http://app.rmsdmedia.com",
 				ws: true,
@@ -20,7 +23,7 @@ module.exports = {
 					"^/api": "",
 				}
 			}
-		}, 
+		},
 		before: app => {}
 	}
 }
