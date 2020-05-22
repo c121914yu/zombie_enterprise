7990900618769f2696e3da8a4f58c8272420c901
yjl <545436317@qq.com>
2020-05-22 22:06:37 +0800

    version 1.0.5

--- src/components/typeTable.vue
+++ src/components/typeTable.vue
@@ -36,25 +36,25 @@ export default{
 					type:"第Ⅱ类",
 					suckBlood:"政府补贴",
 					standard:"grant_prpofit<-0.5\nEBIT<0\nLev>50%",
-					describe:"资产负载率超过50%，日常经营活动能产生利润，但需要靠政府补贴来维持经营状态，从而避免被\"特殊处理\""
+					describe:"资产负债率超过50%，日常经营活动亏损，政府补贴也挽回不了亏损。"
 				},
 				{
 					type:"第Ⅲ类",
 					suckBlood:"常青贷款",
 					standard:"grant_prpofit<1\ndebt_int>0\nEBIT<0\nLev>50%",
-					describe:"资产负载率超过50%，日常经营活动能产生利润，但需要靠政府补贴来维持经营状态，从而避免被\"特殊处理\""
+					describe:"资产负债率超过50%，日常经营活动能产生利润，但息税前利润不足以支付贷款利息，经营状态持续恶化，资产负债率增加。"
 				},
 				{
 					type:"第Ⅳ类",
 					suckBlood:"常青贷款",
 					standard:"inter_cover<1\ndbet_inc>0\nEBIT<0\nLev>50%",
-					describe:"资产负载率超过50%，日常经营活动能产生利润，但需要靠政府补贴来维持经营状态，从而避免被\"特殊处理\""
+					describe:"资产负债率超过50%，日常经营活动亏损，而且息税前利润不足以支付利息，经营状态持续恶化，资产负债率增加。"
 				},
 				{
 					type:"第Ⅴ类",
 					suckBlood:"政府补贴",
 					standard:"dbet_inc>0.3\nprofit_inc<-0.5\ngrant_deficit>-0.5\nEBIT<0",
-					describe:"资产负载率超过50%，日常经营活动能产生利润，但需要靠政府补贴来维持经营状态，从而避免被\"特殊处理\""
+					describe:"日常经营活动亏损，经营状态持续恶化，资产负债率增加，需要政府补贴来弥补一定的亏损经营状态。"
 				}
 			]
 		}
